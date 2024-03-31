bc5ddcf89d92a6e6806c3326db78453d8ce38f4d
skothari07 <skothari855@gmail.com>
2024-03-31 19:40:00 -0400

    Completed A5 till 4.3.5

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
 
 const app = express();
 app.use(cors());
@@ -10,4 +12,6 @@ app.use(express.json());
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 app.listen(4000);
---
+++ assignments/routes.js
@@ -0,0 +1,6 @@
+import db from "../Database/index.js";
+
+function AssignmentRoutes(app) {
+
+}
+export default AssignmentRoutes;
