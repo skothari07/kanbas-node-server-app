723d35258af31dfe2fc1ea124283d39a495e7600
skothari07 <skothari855@gmail.com>
2024-03-31 17:35:37 -0400

    Completed A5 lab ad started Kanbas section

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import express from 'express';
 import cors from "cors";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from './courses/routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 Hello(app);
 Lab5(app);
+CourseRoutes(app);
 app.listen(4000);
