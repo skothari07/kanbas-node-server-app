ec2a8d1263be3040ddb7f1845d02e0e51f0877d2
skothari07 <skothari855@gmail.com>
2024-04-01 01:54:27 -0400

    Completed A5 till section 4

--- modules/routes.js
+++ modules/routes.js
@@ -1,5 +1,6 @@
 import db from "../Database/index.js";
 function ModuleRoutes(app) {
+    
     app.get("/api/courses/:cid/modules", (req, res) => {
         const { cid } = req.params;
         const modules = db.modules
