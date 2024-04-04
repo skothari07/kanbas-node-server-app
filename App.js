fc538c1192b07b549c94e89bc37c2d1452950b14
skothari07 <skothari855@gmail.com>
2024-04-04 12:22:19 -0400

    A6 troubleshooting

--- App.js
+++ App.js
@@ -7,10 +7,29 @@ import ModuleRoutes from './modules/routes.js';
 import AssignmentRoutes from './assignments/routes.js';
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+}));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
 Hello(app);
