379b7c6cf6709d74a9d2427320a617392da3e13f
skothari07 <skothari855@gmail.com>
2024-04-11 00:59:29 -0400

    Cleaned code

--- App.js
+++ App.js
@@ -10,19 +10,14 @@ import UserRoutes from "./Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
 
-const branches = ["main", "a5", "a6", "project"];
+const branches = ["master", "a5", "a6", "project"];
 
 const strippedNetlifyUrl = process.env.NETLIFY_URL.replace("https://", "")
 const allowedOrigins = [process.env.FRONTEND_URL, ...branches.map((branch) => `https://${branch}--${strippedNetlifyUrl}`)];
-console.log(allowedOrigins);
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-/*app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-}));*/
 
 app.use(cors({
     credentials: true,
